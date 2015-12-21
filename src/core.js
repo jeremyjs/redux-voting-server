61f9376aad6f6e4b60d2ced819615e784c1d28d0
Jeremy Meyer <jjman505@gmail.com>
2015-12-21 10:21:42 -0500

    style: foo() {...} => foo () {...}

--- src/core.js
+++ src/core.js
@@ -1,10 +1,10 @@
 import {List, Map} from 'immutable';
 
-export function setEntries(state, entries) {
+export function setEntries (state, entries) {
   return state.set('entries', List(entries));
 }
 
-export function next(state) {
+export function next (state) {
   const entries = state.get('entries');
   return state.merge({
     vote: Map({ pair: entries.take(2) }),
