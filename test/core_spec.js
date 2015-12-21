ba42d5425915e548b7ab3a44087e5abdb3e563ce
Jeremy Meyer <jjman505@gmail.com>
2015-12-21 10:17:13 -0500

    test: ensure vote sets new tallies

--- test/core_spec.js
+++ test/core_spec.js
@@ -1,7 +1,7 @@
-import {List, Map} from 'immutable';
+import {List, Map, fromJS as fromJs} from 'immutable';
 import {expect} from 'chai';
 
-import {setEntries, next} from '../src/core';
+import {setEntries, next, vote} from '../src/core';
 
 describe('application logic', () => {
 
@@ -30,15 +30,38 @@ describe('application logic', () => {
   describe('next', () => {
 
     it('sets the vote to a pair of the next two entries', () => {
-      const state = Map({
-        entries: List.of('Trainspotting', '28 Days Later', 'Sunshine')
+      const state = fromJs({
+        entries: ['Trainspotting', '28 Days Later', 'Sunshine']
       });
       const nextState = next(state);
-      expect(nextState).to.equal(Map({
-        vote: Map({
-          pair: List.of('Trainspotting', '28 Days Later')
-        }),
-        entries: List.of('Sunshine')
+      expect(nextState).to.equal(fromJs({
+        vote: {
+          pair: ['Trainspotting', '28 Days Later']
+        },
+        entries: ['Sunshine']
+      }));
+    });
+
+  });
+
+  describe('vote', () => {
+
+    it('sets the tally for a new vote', () => {
+      const state = fromJs({
+        vote: {
+          pair: ['Trainspotting', '28 Days Later']
+        },
+        entries: []
+      });
+      const nextState = vote(state, 'Trainspotting');
+      expect(nextState).to.equal(fromJs({
+        vote: {
+          pair: ['Trainspotting', '28 Days Later'],
+          tally: {
+            'Trainspotting': 1
+          }
+        },
+        entries: []
       }));
     });
 
