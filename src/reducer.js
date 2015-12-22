80928ae218923a00e25ec804945174d218be8626
Jeremy Meyer <jjman505@gmail.com>
2015-12-22 06:58:34 -0500

    chore: add initial state to reducer

--- src/reducer.js
+++ src/reducer.js
@@ -1,5 +1,5 @@
 import {Map} from 'immutable';
-import {setEntries, next, vote} from './core';
+import {setEntries, next, vote, INITIAL_STATE} from './core';
 
 const actionMap = Map({
   'SET_ENTRIES': setEntries,
@@ -7,7 +7,7 @@ const actionMap = Map({
   'VOTE': vote,
 });
 
-export default function reducer(state, action) {
+export default function reducer(state = INITIAL_STATE, action) {
   const actionType = action.type;
   const actionFunction = actionMap.get(actionType);
   if(actionFunction == null) return state;
