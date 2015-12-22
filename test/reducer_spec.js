6b053b3edaa3a83534bd83a5a52152b7d6362c83
Jeremy Meyer <jjman505@gmail.com>
2015-12-21 23:16:52 -0500

    test: ensure reducer handles NEXT

--- test/reducer_spec.js
+++ test/reducer_spec.js
@@ -15,4 +15,19 @@ describe('reducer', () => {
     }));
   });
 
+  it('handles NEXT', () => {
+    const initialState = fromJS({
+      entries: ['Trainspotting', '28 Days Later']
+    });
+    const action = {type: 'NEXT'};
+    const nextState = reducer(initialState, action);
+
+    expect(nextState).to.equal(fromJS({
+      vote: {
+        pair: ['Trainspotting', '28 Days Later']
+      },
+      entries: []
+    }));
+  });
+
 });
